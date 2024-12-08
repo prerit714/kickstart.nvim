87b806e398ac6f9c85ace0ade53dd63a67fcde22
Prerit Dayal <prerit714@gmail.com>
2024-12-08 14:34:13 +0530

    More setup with grug and oil

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -116,4 +116,50 @@ return {
     'tpope/vim-fugitive',
     lazy = false,
   },
+
+  -- NOTE: Using a good search and replace plugin
+  {
+    'MagicDuck/grug-far.nvim',
+    config = function()
+      require('grug-far').setup {
+        -- options, see Configuration section below
+        -- there are no required options atm
+        -- engine = 'ripgrep' is default, but 'astgrep' can be specified
+      }
+    end,
+  },
+
+  {
+    'stevearc/oil.nvim',
+    config = function()
+      local oil = require 'oil'
+      oil.setup {
+        keymaps = {
+          -- create a new mapping, gs, to search and replace in the current directory
+          gs = {
+            callback = function()
+              -- get the current directory
+              local prefills = { paths = oil.get_current_dir() }
+
+              local grug_far = require 'grug-far'
+              -- instance check
+              if not grug_far.has_instance 'explorer' then
+                grug_far.open {
+                  instanceName = 'explorer',
+                  prefills = prefills,
+                  staticTitle = 'Find and Replace from Explorer',
+                }
+              else
+                grug_far.open_instance 'explorer'
+                -- updating the prefills without clearing the search and other fields
+                grug_far.update_instance_prefills('explorer', prefills, false)
+              end
+            end,
+            desc = 'oil: Search in directory',
+          },
+        },
+        -- rest of your config
+      }
+    end,
+  },
 }
