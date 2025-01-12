66ad216ad219339482d4ceba4ae00e39e78f160f
Prerit Dayal <prerit714@gmail.com>
2025-01-12 13:36:43 +0530

    Using Grug FAR + remove html formatting

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -109,4 +109,15 @@ return {
     'tpope/vim-fugitive',
     lazy = false,
   },
+  -- NOTE: Grug far setup
+  {
+    'MagicDuck/grug-far.nvim',
+    config = function()
+      require('grug-far').setup {
+        -- options, see Configuration section below
+        -- there are no required options atm
+        -- engine = 'ripgrep' is default, but 'astgrep' can be specified
+      }
+    end,
+  },
 }
