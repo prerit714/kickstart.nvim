dfb1b2e3813e549452b97149eff8f79e40cd1909
Prerit Dayal <prerit714@gmail.com>
2025-05-12 18:37:30 +0530

    Treesitter context installed

---
+++ lua/custom/plugins/ts-context.lua
@@ -0,0 +1,18 @@
+return {
+  'nvim-treesitter/nvim-treesitter-context',
+  name = 'treesitter-context',
+  lazy = false,
+  opts = function(_, opts)
+    if type(opts.ensure_installed) == 'table' then
+      -- NOTE: Add http to the list of parsers to install
+      vim.list_extend(opts.ensure_installed, { 'http' })
+    end
+  end,
+  keys = {
+    {
+      '<leader>tc',
+      '<cmd>TSContextToggle<cr>',
+      desc = 'Toggle tree-sitter context',
+    },
+  },
+}
